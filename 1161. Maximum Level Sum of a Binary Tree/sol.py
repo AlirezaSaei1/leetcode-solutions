a648321d466cda1c257688b291336974295c8359
AlirezaSaei1 <saeialireza359@gmail.com>
2026-01-06 21:35:20 +0300

    Added daily challenge solution (1161)

---
+++ 1161. Maximum Level Sum of a Binary Tree/sol.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxLevelSum(self, root):
+        q = deque([root])
+        best_sum = float('-inf')
+        best_level = 1
+        level = 1
+
+        while q:
+            s = 0
+            for _ in range(len(q)):
+                node = q.popleft()
+                s += node.val
+                if node.left: q.append(node.left)
+                if node.right: q.append(node.right)
+
+            if s > best_sum:
+                best_sum = s
+                best_level = level
+
+            level += 1
+
+        return best_level
