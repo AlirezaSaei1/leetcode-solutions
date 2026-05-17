70e926303c2930fea793e7245f29b70b15a81560
AlirezaSaei1 <saeialireza359@gmail.com>
2026-05-17 12:18:58 +0300

    Added daily challenge solution (1306)

---
+++ 1306. Jump Game III/sol.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        seen_idx = set()
+        n = len(arr)
+        
+        def dfs(i):
+            if i < 0 or i >= n:
+                return False
+                
+            if arr[i] == 0:
+                return True
+            
+            if i in seen_idx:
+                return False
+            seen_idx.add(i)
+            
+            return dfs(i - arr[i]) or dfs(i + arr[i])
+
+        return dfs(start)
