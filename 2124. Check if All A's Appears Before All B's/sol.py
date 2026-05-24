08a1da595bda8b126910aeca1c696965ee460212
AlirezaSaei1 <saeialireza359@gmail.com>
2026-05-24 13:00:58 +0300

    Solved problems 2119 and 2124

---
+++ 2124. Check if All A's Appears Before All B's/sol.py
@@ -0,0 +1,11 @@
+class Solution:
+    def checkString(self, s: str) -> bool:
+        seenb = False
+        for char in s:
+            if char == 'a':
+                if seenb:
+                    return False
+            else:
+                seenb = True
+        
+        return True
